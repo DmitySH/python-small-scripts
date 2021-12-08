ffc7ab5859f802be13ad74c7ee82bf2573f2fe77
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-08 11:48:37 +0300

    test_async done

--- test_async.py
+++ test_async.py
@@ -1,5 +1,7 @@
 def make_dict(path='files/java_out.txt'):
     d = dict()
+    test_results = ''
+
     with open(path, 'r') as file:
         for line in file:
             if 'got' in line:
@@ -25,11 +27,27 @@ def make_dict(path='files/java_out.txt'):
                 d[line[0]]['stole'] += int(line[2])
 
             if 'has' in line:
-                print(line, end='')
-    sorted_d = sorted(d.items(), key=lambda x: x[0][-1])
+                words = line.split()
+                d[words[0]] = d.get(words[0],
+                                    {'score': 0, 'from cards': 0,
+                                     'stole': 0})
 
-    return sorted_d
+                if int(words[2]) == int(d[words[0]]['score']):
+                    test_results += line.replace('\n', '') + '  +++OK' + '\n'
+                else:
+                    test_results += line.replace('\n',
+                                                 '') + '  -------ERROR' + '\n'
+
+    return d, test_results
 
 
 if __name__ == '__main__':
-    print('\n'.join(map(lambda x: str(x).replace("'", '')[1:-1], make_dict())))
+    res = make_dict()
+    print('\n'.join(map(lambda x: str(x).replace("'", '')[1:-1],
+                        sorted(res[0].items(), key=lambda x: x[1]['score']))))
+    print()
+    print(res[1] + ("SUCCESS" if 'ERROR' not in res[1] else "ERRORS"))
+    for x in res[1].split('\n'):
+        if 'ERROR' in x:
+            print(x)
+            print(res[0][x.split()[0]])

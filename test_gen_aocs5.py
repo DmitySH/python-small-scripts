2f298930a739d9e4ae414c58037e0a483700452f
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-10 14:01:55 +0300

    file out added

--- test_gen_aocs5.py
+++ test_gen_aocs5.py
@@ -1,6 +1,7 @@
 import random
 
-if __name__ == '__main__':
+
+def console_out():
     for i in range(2):
         if random.randint(1, 100) > 35:
             print(random.randint(1, 50))
@@ -13,3 +14,26 @@ if __name__ == '__main__':
         next_group = random.randint(1, pirates_number)
         print(next_group)
         pirates_number -= next_group
+
+
+def file_out(path):
+    with open(path, 'w') as file:
+        for i in range(2):
+            if random.randint(1, 100) > 35:
+                print(random.randint(1, 50), file=file)
+            else:
+                print(random.randint(1, 100), file=file)
+
+        pirates_number = random.randint(1, 300)
+        print(pirates_number, file=file)
+        while pirates_number > 0:
+            next_group = random.randint(1, pirates_number)
+            print(next_group, file=file)
+            pirates_number -= next_group
+
+
+if __name__ == '__main__':
+    for i in range(1, int(input('Enter number of tests: ')) + 1):
+        file_out(
+            'SOME_PATH\\AoCS-threads\\Task5\\tests\\test{}.txt'
+                .format(i))
